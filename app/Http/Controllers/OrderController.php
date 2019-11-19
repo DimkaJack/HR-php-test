3babaa6a81c268e0b5faf62dc5cfe4b671912eab
D Vorozhtsov <drdouble@mail.ru>
2019-11-19 08:35:29 +0400

    - вывод списка заказов

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Entities\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function orders(Request $request)
+    {
+        $orders = Order::with('orderProducts.product.vendor', 'partner')->get();
+
+        return view('orders.index', compact('orders'));
+    }
+}
---
+++ resources/views/orders/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Orders</div>
+
+                    <div class="panel-body weather-block">
+
+                        <table class="table table-bordered">
+                            <tr>
+                                <th>ID</th>
+                                <th>Partner Name</th>
+                                <th>Amount</th>
+                                <th>Products <br/>Name / price / quantity</th>
+                                <th>Status</th>
+                            </tr>
+                            @forelse ($orders as $order)
+                                <tr>
+                                    <td>{{ $order->id }}</td>
+                                    <td>{{ $order->partnerName }}</td>
+                                    <td>{{ $order->amount() }}</td>
+                                    <td>
+                                        @forelse ($order->orderProducts as $orderProduct)
+                                            {{ $orderProduct->productName }} / {{ $orderProduct->price }} / {{ $orderProduct->quantity }}<br>
+                                        @empty
+                                            <p>No data</p>
+                                        @endforelse
+                                    </td>
+                                    <td>{{ $order->statusName }}</td>
+                                </tr>
+                            @empty
+                                <tr><p>No data</p></tr>
+                            @endforelse
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
